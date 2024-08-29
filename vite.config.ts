3e9d350012bd45ae66ff2dd1aae0758f57c3fbba
basov-roman <basov.roman.work@gmail.com>
2024-08-29 08:47:42 +0300

    Fixed problem with Vercel and Redux.

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,6 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      external: ["react-redux"],
-    },
+    rollupOptions: {},
   },
 });
